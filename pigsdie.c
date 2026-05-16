8a9fb9ecc7719ccb8c6851b7817441582fcef33a
Al Biheiri <al@forgottheaddress.com>
2026-05-15 20:26:24 -0700

    wip

--- pigsdie.c
+++ pigsdie.c
@@ -6,15 +6,29 @@
 int main (void) {
     
     int mary, coke, heron;
+    int health = 100;
+    int money = 100;
+    int day = 1;
+
+    char *cities[] = {"Bronx", "Manhattan", "Queens", "Staten Island", "Brooklyn"};
+    int num_cities = sizeof(cities) / sizeof(cities[0]);
+    char choice;
+    int city_choice;
 
     srand(time(NULL));
-    int randomNumber = (rand() % 3) + 1;
+    int randomNumber = (rand() % 5) + 1;
     printf("%d\n", randomNumber);
 
     if (randomNumber == 1 ){
         printf("Cops! Tuck your butthole\n");
-
     }
+
+    printf("======= Day %d =======\n\n", day);
+
+    printf("==== Stats ====\n");
+    printf("Health: %d\n\n", health);
+
+    printf("WeLcOme 2 dA cItY!\n\n");
 
     int genCoke = 300 + (rand() % 3500);
     printf("%-10s %d\n", "Coke:", genCoke);
@@ -25,5 +39,25 @@ int main (void) {
     int genHeron = 100 + (rand() % 2500);
     printf("%-10s %d\n", "Heroin:", genHeron);
 
-    
+    printf("What do you want to do?\n");
+    printf ("[t]ravel, [b]uy, [s]ell\n");
+
+    scanf(" %c", &choice);
+
+    if (choice == 'c' || choice == 'C') {
+        printf("\nWhere do you want to go?\n");
+        for (int i = 0; i < num_cities; i++) {
+            printf("  %d. %s\n", i + 1, cities[i]);
+        }
+        printf("\nSelect (1-%d): ", num_cities);
+        scanf("%d", &city_choice);
+
+        if (city_choice >= 1 && city_choice <= num_cities) {
+            printf("\nYou traveled to %s.\n", cities[city_choice - 1]);
+        } else {
+            printf("\nInvalid choice.\n");
+        }
+    }
+
+
 }
