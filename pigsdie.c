f090c212be63a721570c2128e344f90e58b37092
Al Biheiri <al@forgottheaddress.com>
2026-05-16 21:17:56 -0700

    Functions and more

--- pigsdie.c
+++ pigsdie.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
+int health = 100;
+int money = 100;
+int day = 1;
+
+void genChoice(void); // Duplicate to tell travel() that it exists
+
 void travel(void) {
 
     char *cities[] = {"Bronx", "Manhattan", "Queens", "Staten Island", "Brooklyn"};
@@ -18,18 +24,54 @@ void travel(void) {
     if (city_choice >= 1 && city_choice <= num_cities) {
         printf("\nYou traveled to %s.\n", cities[city_choice - 1]);
     } else {
-        printf("\nInvalid choice.\n");
+        int c; // Clear buffer
+        while ((c = getchar()) != '\n' && c != EOF) { } // Clear buffer
+        genChoice();
+    }
+}
+
+void genDrugs(void){
+
+    int genCoke = 300 + (rand() % 3500);
+    printf("%-10s %d\n", "Coke:", genCoke);
+
+    int genMary = 80 + (rand() % 1200);
+    printf("%-10s %d\n", "Weed:", genMary);
+
+    int genHeron = 100 + (rand() % 2500);
+    printf("%-10s %d\n", "Heroin:", genHeron);
+
+}
+
+void buy(void){
+
+}
+
+void sell(void){
+
+}
+
+void genChoice(void){
+    char choice;
+
+    printf("What do you want to do?\n");
+    printf ("[t]ravel, [b]uy, [s]ell\n");
+
+    scanf(" %c", &choice);
+
+    if (choice == 't' || choice == 'T') {
+        travel();
+    } 
+    else if  (choice == 'b' || choice == 'B') {
+        buy();
     }
 }
 
 int main (void) {
     
     int mary, coke, heron;
-    int health = 100;
-    int money = 100;
-    int day = 1;
 
-    char choice;
+
 
     srand(time(NULL));
     int randomNumber = (rand() % 5) + 1;
@@ -41,28 +83,14 @@ int main (void) {
 
     printf("======= Day %d =======\n\n", day);
 
-    printf("==== Stats ====\n");
-    printf("Health: %d\n\n", health);
+    printf("Health: %d\n", health);
+    printf("Money: %d\n\n", money);
 
     printf("WeLcOme 2 dA cItY!\n\n");
 
-    int genCoke = 300 + (rand() % 3500);
-    printf("%-10s %d\n", "Coke:", genCoke);
+    genDrugs();
+    genChoice();
 
-    int genMary = 80 + (rand() % 1200);
-    printf("%-10s %d\n", "Weed:", genMary);
-
-    int genHeron = 100 + (rand() % 2500);
-    printf("%-10s %d\n", "Heroin:", genHeron);
-
-    printf("What do you want to do?\n");
-    printf ("[t]ravel, [b]uy, [s]ell\n");
-
-    scanf(" %c", &choice);
-
-    if (choice == 't' || choice == 't') {
-        travel();
-    }
 
 
 }
