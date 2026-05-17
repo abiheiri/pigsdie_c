75d9abe466bf0a3ccf8c574215145f86a7307727
Al <al@forgottheaddress.com>
2026-05-17 13:36:09 -0700

    Buy buy buy!

--- pigsdie.c
+++ pigsdie.c
@@ -3,28 +3,48 @@
 #include <time.h>
 
 int health = 100;
-int money = 100;
+int money = 1000;
 int day = 1;
 int bullets = 0;
-int mary, coke, heron = 0;
+int weed, coke, heroin = 0;
+int my_weed, my_coke, my_heroin = 0;
+int pocket_space = 10;
+int pocket_used = 0;
+int level, skill = 1;
+
+void genChoice(void); // Duplicate to tell travel() that it exists
 
 void clearScreen() {
     printf("\033[2J\033[H");  // Clear screen and move cursor to top-left
     fflush(stdout);            // Ensure it prints immediately
 }
 
-void genChoice(void); // Duplicate to tell travel() that it exists
+int readInt(const char *prompt) { // We validate the user is passing an int
+    int value;
+    int result;
+    while (1) {
+        printf("%s", prompt);
+        result = scanf("%d", &value);
+        if (result == 1) {
+            return value;
+        }
+        printf("Invalid input. Please enter a number.\n");
+        // Clear buffer
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF) {}
+    }
+}
 
 void genDrugs(void){
 
-    int genCoke = 300 + (rand() % 3500);
-    printf("%-10s %d\n", "Coke:", genCoke);
+    coke = 300 + (rand() % 3500);
+    printf("%-10s %d\n", "Coke:", coke);
 
-    int genMary = 80 + (rand() % 1200);
-    printf("%-10s %d\n", "Weed:", genMary);
+    weed = 80 + (rand() % 800);
+    printf("%-10s %d\n", "Weed:", weed);
 
-    int genHeron = 100 + (rand() % 2500);
-    printf("%-10s %d\n", "Heroin:", genHeron);
+    heroin = 100 + (rand() % 2500);
+    printf("%-10s %d\n", "Heroin:", heroin);
 
 }
 
@@ -37,7 +57,10 @@ void genStats(void){
 
 void genInventory(void){
     printf("Your inventory is:\n");
-    printf("Weed: %d\nCoke: %d\nHeroin: %d\nBullets: %d\n", mary, coke, heron, bullets);
+    printf("====\n");
+    printf("Weed: %d\nCoke: %d\nHeroin: %d\nBullets: %d\n", my_weed, my_coke, my_heroin, bullets);
+    printf("====\n");
+    printf("Pocket space: %d\nPockets used: %d\nMoney: %d\nHealth: %d", pocket_space, pocket_used, money, health);
 }
 
 void genCops(void){
@@ -50,7 +73,7 @@ void genCops(void){
 
         while (1) {
             printf("Shit, its the cops! What should we do?\n");
-            printf("[r]un, [s]hoot them, [g]ive up, [q]uit\n");
+            printf("[r]un, [s]hoot them, [g]ive up\n");
 
             scanf(" %c", &choice);
 
@@ -63,13 +86,9 @@ void genCops(void){
             else if (choice == 'g' || choice == 'G') {
                 printf ("Cops took all your stuff and held you in prison for 10 days.\n");
                 day = day + 10;
-                mary = 0; 
-                coke = 0;
-                heron = 0;
-                genChoice();
-            }
-            else if (choice == 'q' || choice == 'Q') {
-                printf("See ya!\n");
+                my_weed = 0; 
+                my_coke = 0;
+                my_heroin = 0;
                 break;
             }
             else {
@@ -81,6 +100,13 @@ void genCops(void){
 
 void travel(void) {
 
+    if ( day >= 30 ) {
+        printf ("!!! GaMe OvEr !!!\n");
+        printf ("Results:\n");
+        genStats();
+        exit(0);
+    }
+
     char *cities[] = {"Bronx", "Manhattan", "Queens", "Staten Island", "Brooklyn"};
     int num_cities = sizeof(cities) / sizeof(cities[0]);
     int city_choice;
@@ -90,7 +116,7 @@ void travel(void) {
         printf("  %d. %s\n", i + 1, cities[i]);
     }
     printf("\nSelect (1-%d): ", num_cities);
-    scanf("%d", &city_choice);
+    city_choice = readInt("");
 
     if (city_choice >= 1 && city_choice <= num_cities) {
         day = day + 1;
@@ -108,22 +134,92 @@ void travel(void) {
 
 }
 
-
 void buy(void){
+    char choice;
+    int num, result, response;
 
+    while (1) {
+        printf("\nWhat do you want to buy?\n");
+        printf("[w]eed, [c]oke, [h]erion, [b]ullets, [l]eave\n");
+
+        scanf(" %c", &choice);
+
+        if (choice == 'w' || choice == 'W') {
+            printf("How many do you want to buy?\n");
+            response = readInt(""); // Validate its a number
+            
+            if (response > pocket_space) {
+                printf("You dont have the space for %d. You only have %d pocket space.\n", response, pocket_space);
+            }
+            else if (weed > money){
+                printf("You dont have enough money to buy. It costs %d and you have $%d\n", weed, money);
+            }
+            else {
+                my_weed = my_weed + response;
+                pocket_used = pocket_used + response;
+                pocket_space = pocket_space - response;
+                money = money - (response * weed);
+                skill += 10;
+            }
+        }
+        else if (choice == 'c' || choice == 'C') {
+
+            printf("How many do you want to buy?\n");
+            response = readInt(""); // Validate its a number
+            
+            if (response > pocket_space) {
+                printf("You dont have the space for %d. You only have %d pocket space.\n", response, pocket_space);
+            }
+            else if (weed > money){
+                printf("You dont have enough money to buy. It costs %d and you have $%d\n", coke, money);
+            }
+            else {
+                my_coke = my_coke + response;
+                pocket_used = pocket_used + response;
+                pocket_space = pocket_space - response;
+                money = money - (response * coke);
+                skill += 20;
+            }
+
+        }
+        else if (choice == 'h' || choice == 'H') {
+
+            printf("How many do you want to buy?\n");
+            response = readInt(""); // Validate its a number
+            
+            if (response > pocket_space) {
+                printf("You dont have the space for %d. You only have %d pocket space.\n", response, pocket_space);
+            }
+            else if (weed > money){
+                printf("You dont have enough money to buy. It costs %d and you have $%d\n", heroin, money);
+            }
+            else {
+                my_heroin = my_heroin + response;
+                pocket_used = pocket_used + response;
+                pocket_space = pocket_space - response;
+                money = money - (response * heroin);
+                skill += 20;
+            }
+
+        }
+        else if (choice == 'b' || choice == 'B') {
+            break;
+        }
+        else if (choice == 'l' || choice == 'l') {
+            printf("See ya!\n");
+            break;
+        }
+        else {
+            printf("Invalid choice.\n");
+        }
+    }
 }
 
 void sell(void){
-
+    
 }
 
 void genChoice(void) {
-
-    if ( day >= 30 ) {
-        printf ("!!! GaMe OvEr !!!\n");
-        printf ("Results:\n");
-        exit(0);
-    }
     
     char choice;
 
